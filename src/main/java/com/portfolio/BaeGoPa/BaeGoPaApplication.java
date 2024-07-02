29b4bcc94bee97b2b715bd61af9de0e3fe1328b9
이창민 <2ckdaks@gmail.com>
2024-07-03 02:45:02 +0900

    first init

---
+++ src/main/java/com/portfolio/BaeGoPa/BaeGoPaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.BaeGoPa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeGoPaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeGoPaApplication.class, args);
+	}
+
+}
